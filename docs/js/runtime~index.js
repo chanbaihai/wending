d789ffdb667e010bd3d9cd79b75d86ee4e193421
chanbaihai <chanbaihai@outlook.com>
2018-07-23 16:22:36 +0800

    设计修改

--- docs/js/runtime~index.js
+++ docs/js/runtime~index.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0408ad67e9ecec6ebab5";
+/******/ 	var hotCurrentHash = "bfc06f016e250ae72e48";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
