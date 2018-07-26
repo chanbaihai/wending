ea7e84b4e0b4bbf1198f5ccc674f8763bbfbd490
chanbaihai <chanbaihai@outlook.com>
2018-07-26 10:03:29 +0800

    加大报名流程字体22px

--- docs/js/join.js
+++ docs/js/join.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ae7aef8e8939ee0ae3fc";
+/******/ 	var hotCurrentHash = "5cf4325cbd9f184674ac";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
